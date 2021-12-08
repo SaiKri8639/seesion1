1744a55d90627390d48e6535c7af6f29aa86819b
Sai Krishna Neeli <saikrishnaneeli98@gmail.com>
2021-12-08 19:21:46 +0530

    pushing changes to remote

---
+++ UserManagementModule.java
@@ -0,0 +1,11 @@
+package allConceptsPrep;
+
+public class UserManagementModule 
+{
+  public static void main(String[] args) 
+  {
+	UserManager u = new UserManager();
+    u.createNewUser();
+	u.getUserById();
+  }
+}
