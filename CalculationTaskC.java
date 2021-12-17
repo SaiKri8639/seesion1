11a77dc05da38f59795857890a477c6ad697207f
Sai Krishna Neeli <saikrishnaneeli98@gmail.com>
2021-12-17 18:58:33 +0530

    Pushing changes to remote

---
+++ CalculationTaskC.java
@@ -0,0 +1,27 @@
+package threadsCreationAndManagement;
+
+	import java.util.concurrent.Callable;
+	public class CalculationTaskC implements Callable<Integer>
+	{
+	private String name="Thread_";
+	private int count =0;
+//	private int data[];
+
+	@Override
+	public Integer call() throws Exception
+	{
+	// String ThreadName = Thread.currentThread().getName();
+	System.out.println(Thread.currentThread().getName()+"Started working");
+	for(int i=0;i<10;i++)
+	{
+	System.out.println(Thread.currentThread().getName()+" "+i);
+	}
+	System.out.println(Thread.currentThread().getName() +"stopped working");
+	return 0;
+	}
+	public CalculationTaskC()
+	{
+		count++;
+		name+=count;
+	}
+	}
