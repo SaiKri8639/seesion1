94ba29bad8660fa7d4455ce77562a9d05affeab8
SaiKri8639 <94732480+SaiKri8639@users.noreply.github.com>
2021-12-29 19:49:36 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package persistent.session.BankCardConfig;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
